a453f7ff7a54f375e7e710a11e33da4944b66459
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-10 11:30:31 +0700

    Update dataset - rearrange entries - add getter method - fix lesson size

--- src/main/java/model/dataset/core/Dataset.java
+++ src/main/java/model/dataset/core/Dataset.java
@@ -2,8 +2,11 @@ package model.dataset.core;
 
 import model.database.loader.DBProblemLoader;
 import model.dataset.component.DSLesson;
+import model.dataset.component.DSLessonCluster;
+import model.dataset.component.DSLessonGroup;
 import model.dataset.component.DSTimeOff;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /*
  * This <skripsi.hdpso.scheduling> project in package <model.dataset.core> created by : 
@@ -14,13 +17,17 @@ import org.jetbrains.annotations.NotNull;
  */
 @SuppressWarnings({"FieldCanBeLocal", "unused"}) public class Dataset
 {
-    private final @NotNull int[]       days;
-    private final @NotNull int[]       periods;
-    private final @NotNull DSTimeOff[] classes;
-    private final @NotNull DSTimeOff[] classrooms;
-    private final @NotNull DSTimeOff[] lecturers;
-    private final @NotNull DSTimeOff[] subjects;
-    private final @NotNull DSLesson[]  lessons;
+    private final @NotNull int[]             days;
+    private final @NotNull int[]             periods;
+    private final @NotNull int[]             time_distribution;
+    private final @NotNull int[][][]         clustered_classroom_time;
+    private final @NotNull DSTimeOff[]       classes;
+    private final @NotNull DSTimeOff[]       classrooms;
+    private final @NotNull DSTimeOff[]       lecturers;
+    private final @NotNull DSTimeOff[]       subjects;
+    private final @NotNull DSLesson[]        lessons;
+    private final @NotNull DSLessonGroup[]   lesson_group;
+    private final @NotNull DSLessonCluster[] lesson_cluster;
 
     public Dataset(final @NotNull DBProblemLoader loader)
     {
@@ -30,17 +37,26 @@ import org.jetbrains.annotations.NotNull;
         this.classrooms = new DSTimeOff[loader.getOperatingClassroom().size()];
         this.lecturers = new DSTimeOff[loader.getOperatingLecture().size()];
         this.subjects = new DSTimeOff[loader.getOperatingSubject().size()];
-        this.lessons = new DSLesson[(this.days.length * this.periods.length * loader.getOperatingClassroom().size()) - (loader.getTotalRegisteredTime())];
+        this.lessons = new DSLesson[loader.getOperatingClassroomAllowedLesson() - loader.getTotalRegisteredTime() + loader.getComplexLessonSize()];
+        this.time_distribution = new int[loader.getTimeDistribution().size()];
+        this.lesson_group = new DSLessonGroup[loader.getLessonGroup().size()];
+        this.lesson_cluster = new DSLessonCluster[loader.getLessonCluster().size()];
+        this.clustered_classroom_time = new int[this.classrooms.length][this.days.length][];
     }
 
-    public @NotNull int[] getDays()
+    @NotNull public int[] getDays()
     {
         return this.days;
     }
 
-    public @NotNull int[] getPeriods()
+    @NotNull public int[] getPeriods()
     {
         return this.periods;
+    }
+
+    @NotNull public int[] getTimeDistribution()
+    {
+        return this.time_distribution;
     }
 
     @NotNull public DSTimeOff[] getClasses()
@@ -66,5 +82,55 @@ import org.jetbrains.annotations.NotNull;
     @NotNull public DSLesson[] getLessons()
     {
         return this.lessons;
+    }
+
+    @NotNull public DSLessonGroup[] getLessonGroups()
+    {
+        return this.lesson_group;
+    }
+
+    @NotNull public DSLessonCluster[] getLessonClusters()
+    {
+        return this.lesson_cluster;
+    }
+
+    @NotNull public DSTimeOff getKlass(final int klass_id)
+    {
+        return this.classes[klass_id];
+    }
+
+    @NotNull public DSTimeOff getClassroom(final int classroom_id)
+    {
+        return this.classrooms[classroom_id];
+    }
+
+    @NotNull public DSTimeOff getLecturer(final int lecturer_id)
+    {
+        return this.lecturers[lecturer_id];
+    }
+
+    @NotNull public DSTimeOff getSubject(final int subject_id)
+    {
+        return this.subjects[subject_id];
+    }
+
+    @Nullable public DSLesson getLesson(final int lesson_id)
+    {
+        return this.lessons[lesson_id];
+    }
+
+    @NotNull public DSLessonGroup getLessonGroup(final int lesson_group_id)
+    {
+        return this.lesson_group[lesson_group_id];
+    }
+
+    @NotNull public DSLessonCluster getLessonClusters(final int lesson_cluster_id)
+    {
+        return this.lesson_cluster[lesson_cluster_id];
+    }
+
+    @NotNull public int[][][] getClusteredClassroomTime()
+    {
+        return this.clustered_classroom_time;
     }
 }
