871f38b1a8dec4b06edbaac3de0446073e5c8d9e
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-15 15:36:48 +0700

    Add velocity testing for addition with benchmark

---
+++ src/test/java/model/method/pso/hdpso/component/TVelocity_AdditionVelocity_Benchmark.java
@@ -0,0 +1,55 @@
+package model.method.pso.hdpso.component;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+ * This <skripsi.hdpso.scheduling> project in package <model.method.pso.hdpso.component> created by : 
+ * Name         : syafiq
+ * Date / Time  : 15 November 2016, 3:30 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class TVelocity_AdditionVelocity_Benchmark
+{
+    private Velocity destination;
+    private Velocity source;
+
+    @Before
+    public void initialize()
+    {
+        final int SIZE   = 20000000;
+        final int SEED   = 100;
+        Random    random = ThreadLocalRandom.current();
+        this.destination = new Velocity(SIZE);
+        for(int ci = -1, cis = SIZE / 2; ++ci < cis; )
+        {
+            destination.set(random.nextInt(SEED), random.nextInt(SEED));
+        }
+        System.out.println(destination.size());
+        this.source = new Velocity(SIZE);
+        for(int ci = -1, cis = SIZE / 2; ++ci < cis; )
+        {
+            source.set(random.nextInt(SEED), random.nextInt(SEED));
+        }
+        System.out.println(source.size());
+    }
+
+    @Test
+    public void destination_lt_source_no_reduction()
+    {
+
+        Long time = System.currentTimeMillis();
+        Velocity.additionVelocity(this.destination, this.source);
+        System.out.println(System.currentTimeMillis() - time);
+    }
+
+    @After
+    public void finish()
+    {
+        System.out.println(this.destination.size());
+    }
+}
