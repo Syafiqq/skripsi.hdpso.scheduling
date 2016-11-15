620bc6017fd27df5c2b64640b32de87d410af24c
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-15 15:05:09 +0700

    Add Velocity class object

---
+++ src/test/java/model/method/pso/hdpso/component/TVelocity.java
@@ -0,0 +1,12 @@
+package model.method.pso.hdpso.component;
+
+/*
+ * This <skripsi.hdpso.scheduling> project in package <model.method.pso.hdpso.component> created by : 
+ * Name         : syafiq
+ * Date / Time  : 15 November 2016, 3:04 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class TVelocity
+{
+}
