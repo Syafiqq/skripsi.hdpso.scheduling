bdad75ca51228ca01e548d5810ae5efd7710afb5
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-15 16:02:34 +0700

    Update velocity testing for addition with benchmark

--- src/test/java/model/method/pso/hdpso/component/TVelocity_AdditionVelocity_Benchmark.java
+++ src/test/java/model/method/pso/hdpso/component/TVelocity_AdditionVelocity_Benchmark.java
@@ -2,6 +2,8 @@ package model.method.pso.hdpso.component;
 
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /*
@@ -13,25 +15,41 @@ import org.junit.Test;
  */
 public class TVelocity_AdditionVelocity_Benchmark
 {
+    private Velocity destination;
+    private Velocity source;
+
+    @Before
+    public void initialize()
+    {
+        final int SIZE   = 30000000;
+        final int SEED   = 100;
+        Random    random = ThreadLocalRandom.current();
+        this.destination = new Velocity(SIZE);
+        for(int ci = -1, cis = SIZE / 2; ++ci < cis; )
+        {
+            destination.set(ci, ci);
+        }
+        System.out.println(destination.size());
+        this.source = new Velocity(SIZE);
+        for(int ci = -1, cis = SIZE / 2; ++ci < cis; )
+        {
+            source.set(ci, ci);
+        }
+        System.out.println(source.size());
+    }
+
     @Test
     public void destination_lt_source_no_reduction()
     {
-        final int SIZE        = 10000000;
-        final int SEED        = 100;
-        Random    random      = ThreadLocalRandom.current();
-        Velocity  destination = new Velocity(SIZE);
-        for(int ci = -1, cis = SIZE / 2; ++ci < cis; )
-        {
-            destination.set(random.nextInt(SEED), random.nextInt(SEED));
-        }
-        Velocity source = new Velocity(SIZE);
-        for(int ci = -1, cis = SIZE / 2; ++ci < cis; )
-        {
-            source.set(random.nextInt(SEED), random.nextInt(SEED));
-        }
-        System.out.println(source.size());
-        System.out.println(destination.size());
-        Velocity.additionVelocity(destination, source);
-        System.out.println(destination.size());
+
+        Long time = System.currentTimeMillis();
+        Velocity.additionVelocity(this.destination, this.source);
+        System.out.println(System.currentTimeMillis() - time);
+    }
+
+    @After
+    public void finish()
+    {
+        System.out.println(this.destination.size());
     }
 }
