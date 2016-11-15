91edd1c3043e0d1b99512604cf1a1b06b8288c15
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-15 16:31:39 +0700

    Add velocity testing for reverse

---
+++ src/test/java/model/method/pso/hdpso/component/TVelocity_ReverseVelocity_Benchmark.java
@@ -0,0 +1,44 @@
+package model.method.pso.hdpso.component;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+ * This <skripsi.hdpso.scheduling> project in package <model.method.pso.hdpso.component> created by : 
+ * Name         : syafiq
+ * Date / Time  : 15 November 2016, 4:27 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class TVelocity_ReverseVelocity_Benchmark
+{
+    private Velocity destination;
+
+    @Before
+    public void initialize()
+    {
+        final int SIZE = 30000000;
+        this.destination = new Velocity(SIZE);
+        for(int ci = -1; ++ci < SIZE; )
+        {
+            destination.set(ci, ci);
+        }
+        System.out.println(destination.size());
+    }
+
+    @Test
+    public void destination_lt_source_no_reduction()
+    {
+
+        Long time = System.currentTimeMillis();
+        Velocity.reverseVelocity(this.destination);
+        System.out.println(System.currentTimeMillis() - time);
+    }
+
+    @After
+    public void finish()
+    {
+        System.out.println(this.destination.size());
+    }
+}
