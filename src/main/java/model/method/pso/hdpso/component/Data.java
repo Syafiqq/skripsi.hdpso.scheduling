e4db1264859952c0b603822cbb66baac83d7b0ce
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-22 09:44:15 +0700

    Add data class

---
+++ src/main/java/model/method/pso/hdpso/component/Data.java
@@ -0,0 +1,65 @@
+package model.method.pso.hdpso.component;
+
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This <skripsi.hdpso.scheduling> project in package <model.method.pso.hdpso.component> created by : 
+ * Name         : syafiq
+ * Date / Time  : 17 November 2016, 3:30 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+@SuppressWarnings({"unused", "WeakerAccess"}) public class Data
+{
+    @NotNull private final Position[] positions;
+    private                double     fitness;
+
+    public Data(@NotNull final Position[] positions)
+    {
+        this.positions = positions;
+        this.fitness = 0.0;
+    }
+
+    public static Data newInstance(@NotNull final Data data)
+    {
+        @NotNull final Position[] positions = new Position[data.positions.length];
+        for(int c_position = -1, c_position_s = positions.length; ++c_position < c_position_s; )
+        {
+            positions[c_position] = Position.newInstance(data.positions[c_position].getPosition());
+        }
+        return new Data(positions);
+    }
+
+    public static void replaceData(final Data destination, final Data source)
+    {
+        Data.replacePosition(destination.positions, source.positions);
+        destination.fitness = source.fitness;
+    }
+
+    public static void replacePosition(final Position[] destination, final Position[] source)
+    {
+        for(int counter_position = -1, position_size = source.length; ++counter_position < position_size; )
+        {
+            Position.replace(destination[counter_position], source[counter_position]);
+        }
+    }
+
+    public String toString(int indent)
+    {
+        String storedIndent = "";
+        for(int i = indent; --i >= 0; )
+        {
+            storedIndent += '\t';
+        }
+
+        return String.format("%s{\n", storedIndent) +
+                String.format("%s", Position.toString(this.positions, indent + 1)) +
+                String.format("%s\t%12s : %g\n", storedIndent, "Fitness", this.fitness) +
+                String.format("%s}", storedIndent);
+    }
+
+    @Override public String toString()
+    {
+        return this.toString(0);
+    }
+}
