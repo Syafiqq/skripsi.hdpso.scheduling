019066dea63206bdd856f617aa0b6e6171322426
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-26 06:10:24 +0700

    -Add time distribution -Add lesson null

--- src/main/java/model/dataset/component/DSScheduleShufflingProperty.java
+++ src/main/java/model/dataset/component/DSScheduleShufflingProperty.java
@@ -1,5 +1,9 @@
 package model.dataset.component;
 
+import it.unimi.dsi.fastutil.ints.IntArrays;
+import java.util.Random;
+import org.jetbrains.annotations.NotNull;
+
 /*
  * This <skripsi.hdpso.scheduling> project in package <model.dataset.component> created by : 
  * Name         : syafiq
@@ -9,10 +13,18 @@ package model.dataset.component;
  */
 @SuppressWarnings({"unused", "WeakerAccess"}) public class DSScheduleShufflingProperty
 {
+    @NotNull public final  Random      random;
     /*
+    * @param time_distribution
+    *   index
+    *       1 : time index
     * @param day_set
     *   index
     *       1 : day position order
+    * @param lesson_null_set
+    *   index
+    *       1 : cluster index
+    *       2 : lesson null entry
     * @param classroom_set
     *   index
     *       1 : cluster index
@@ -23,45 +35,54 @@ package model.dataset.component;
     *       1 : cluster index
     *       2 : group index
     *       3 : lessons
-    * @param clustered_classroom_property_set
+    * @param lesson_appender_manager
     *   index
     *       1 : cluster index
     *       2 : classroom index
     *       3 : day index
     *       4 : clustered_time
     * */
-    private final int[]       day_set;
-    private final int[][][]   classrooms_set;
-    private final int[][][]   lessons_set;
-    private final int[][][][] clustered_classroom_property_set;
+    @NotNull private final int[]       time_distribution;
+    @NotNull private final int[]       day_set;
+    @NotNull private final int[][]     lesson_null_set;
+    @NotNull private final int[][][]   classrooms_set;
+    @NotNull private final int[][][]   lessons_set;
+    @NotNull private final int[][][][] lesson_appender_manager;
 
-    public DSScheduleShufflingProperty(int[] day_set, int[][][] classrooms_set, int[][][] lessons_set, int[][][][] clustered_classroom_property_set)
+    public DSScheduleShufflingProperty(@NotNull int[] day_set, @NotNull int[][] lesson_null_set, @NotNull int[][][] classrooms_set, @NotNull int[][][] lessons_set, @NotNull int[][][][] clustered_classroom_property_set, @NotNull final int[] time_distribution)
     {
         this.day_set = day_set;
+        this.lesson_null_set = lesson_null_set;
         this.classrooms_set = classrooms_set;
         this.lessons_set = lessons_set;
-        this.clustered_classroom_property_set = clustered_classroom_property_set;
+        this.lesson_appender_manager = clustered_classroom_property_set;
+        this.time_distribution = time_distribution;
+        this.random = new Random();
     }
 
     public static DSScheduleShufflingProperty newInstance(DSScheduleShufflingProperty properties)
     {
         final int[]       day_set_source                           = properties.day_set;
         final int[]       day_set_destination                      = new int[properties.day_set.length];
+        final int[][]     lesson_null_set_source                   = properties.lesson_null_set;
+        final int[][]     lesson_null_set_destination              = new int[properties.lesson_null_set.length][];
         final int[][][]   classroom_set_source                     = properties.classrooms_set;
         final int[][][]   classroom_set_destination                = new int[properties.classrooms_set.length][][];
         final int[][][]   lesson_set_source                        = properties.lessons_set;
         final int[][][]   lesson_set_destination                   = new int[properties.lessons_set.length][][];
-        final int[][][][] clustered_classroom_property_source      = properties.clustered_classroom_property_set;
-        final int[][][][] clustered_classroom_property_destination = new int[properties.clustered_classroom_property_set.length][][][];
+        final int[][][][] clustered_classroom_property_source      = properties.lesson_appender_manager;
+        final int[][][][] clustered_classroom_property_destination = new int[properties.lesson_appender_manager.length][][][];
 
         System.arraycopy(day_set_source, 0, day_set_destination, 0, day_set_source.length);
 
         for(int c_cluster = -1, c_cluster_s = classroom_set_source.length; ++c_cluster < c_cluster_s; )
         {
             classroom_set_destination[c_cluster] = new int[classroom_set_source[c_cluster].length][];
+            lesson_null_set_destination[c_cluster] = new int[lesson_null_set_source[c_cluster].length];
             lesson_set_destination[c_cluster] = new int[lesson_set_source[c_cluster].length][];
             clustered_classroom_property_destination[c_cluster] = new int[clustered_classroom_property_source[c_cluster].length][][];
 
+            System.arraycopy(lesson_null_set_source[c_cluster], 0, lesson_null_set_destination[c_cluster], 0, lesson_null_set_destination[c_cluster].length);
             for(int counter_entity_child = -1, entity_child_size = classroom_set_source[c_cluster].length; ++counter_entity_child < entity_child_size; )
             {
                 classroom_set_destination[c_cluster][counter_entity_child] = new int[classroom_set_source[c_cluster][counter_entity_child].length];
@@ -77,13 +98,13 @@ package model.dataset.component;
             }
         }
 
-        return new DSScheduleShufflingProperty(day_set_destination, classroom_set_destination, lesson_set_destination, clustered_classroom_property_destination);
+        return new DSScheduleShufflingProperty(day_set_destination, lesson_null_set_destination, classroom_set_destination, lesson_set_destination, clustered_classroom_property_destination, IntArrays.copy(properties.time_distribution));
     }
 
 
     public void reset_classroom_current_time()
     {
-        for(int[][][] lesson_pool : this.clustered_classroom_property_set)
+        for(int[][][] lesson_pool : this.lesson_appender_manager)
         {
             for(int[][] classrooms : lesson_pool)
             {
@@ -98,9 +119,9 @@ package model.dataset.component;
         }
     }
 
-    public void reset_classroom_current_time(int pool_index)
+    public void reset_classroom_current_time(final int pool_index)
     {
-        for(int[][] classrooms : this.clustered_classroom_property_set[pool_index])
+        for(int[][] classrooms : this.lesson_appender_manager[pool_index])
         {
             for(int[] day : classrooms)
             {
@@ -110,5 +131,35 @@ package model.dataset.component;
                 }
             }
         }
+    }
+
+    public int[] getDaySet()
+    {
+        return this.day_set;
+    }
+
+    public int[][][] getLessonAppenderManager(final int cluster)
+    {
+        return this.lesson_appender_manager[cluster];
+    }
+
+    public int[] getLessonSet(final int cluster, final int group)
+    {
+        return this.lessons_set[cluster][group];
+    }
+
+    public int[] getClassroomSet(final int cluster, final int group)
+    {
+        return this.classrooms_set[cluster][group];
+    }
+
+    public int[] getTimeDistribution()
+    {
+        return this.time_distribution;
+    }
+
+    public int[] getLessonNullSet(final int cluster)
+    {
+        return this.lesson_null_set[cluster];
     }
 }
