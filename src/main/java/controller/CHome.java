36ad0ec59250575a2db5ba512850b5085f212f95
Syafiqq <syafiq.rezpector@gmail.com>
2017-01-01 17:16:49 +0700

    add container

---
+++ src/main/java/controller/CHome.java
@@ -0,0 +1,85 @@
+package controller;
+
+import controller.menu.CMCategory;
+import controller.menu.CMDeveloper;
+import controller.menu.CMHome;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import view.menu.IMCategory;
+import view.menu.IMDeveloper;
+import view.menu.IMHome;
+
+/*
+ * This <skripsi.hdpso.scheduling> project in package <controller> created by : 
+ * Name         : syafiq
+ * Date / Time  : 01 January 2017, 2:18 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class CHome implements Initializable
+{
+    @FXML public BorderPane home_ribbon;
+    @FXML public Button     home_menu_home;
+    @FXML public Button     home_menu_configuration;
+    @FXML public Button     home_menu_development;
+
+    @Override public void initialize(URL location, ResourceBundle resources)
+    {
+        try
+        {
+            this.setRibbon(IMHome.load(new CMHome()).load());
+        }
+        catch(IOException ignored)
+        {
+
+        }
+    }
+
+    private void setRibbon(Node node)
+    {
+        this.home_ribbon.setBottom(null);
+        this.home_ribbon.setBottom(node);
+    }
+
+    public void onHomeClick()
+    {
+        try
+        {
+            this.setRibbon(IMHome.load(new CMHome()).load());
+        }
+        catch(IOException ignored)
+        {
+
+        }
+    }
+
+    public void onConfigurationClick()
+    {
+        try
+        {
+            this.setRibbon(IMCategory.load(new CMCategory()).load());
+        }
+        catch(IOException ignored)
+        {
+
+        }
+    }
+
+    public void onDevelopmentClick()
+    {
+        try
+        {
+            this.setRibbon(IMDeveloper.load(new CMDeveloper()).load());
+        }
+        catch(IOException ignored)
+        {
+
+        }
+    }
+}
