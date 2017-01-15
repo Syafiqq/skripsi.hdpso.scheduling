80f4819de97df0c4ec34c1ccbac14cf39a787581
Syafiqq <syafiq.rezpector@gmail.com>
2017-01-15 22:01:07 +0700

    add Exit Button Functionality

--- src/main/java/controller/menu/CMHome.java
+++ src/main/java/controller/menu/CMHome.java
@@ -2,7 +2,9 @@ package controller.menu;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 
 /*
  * This <skripsi.hdpso.scheduling> project in package <controller> created by : 
@@ -16,5 +18,10 @@ public class CMHome implements Initializable
     @Override public void initialize(URL location, ResourceBundle resources)
     {
 
+    }
+
+    public void onButtonExitPressed(ActionEvent event)
+    {
+        ((Node) event.getSource()).getScene().getWindow().hide();
     }
 }
