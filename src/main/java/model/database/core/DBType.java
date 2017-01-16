37129f9c3ce47961fa06359fcff346601a0abfec
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2017-01-16 10:40:19 +0700

    add db specification

---
+++ src/main/java/model/database/core/DBType.java
@@ -0,0 +1,12 @@
+package model.database.core;/*
+ * This <skripsi.hdpso.scheduling> project created by : 
+ * Name         : Muhammad Syafiq
+ * Date / Time  : 16 January 2017, 5:47 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+public enum DBType {
+    DEFAULT,
+    JAR
+}
