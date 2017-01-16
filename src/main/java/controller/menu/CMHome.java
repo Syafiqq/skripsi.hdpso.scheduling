b9ee39993bef8b9416a28683ffe1440eb5f4f25f
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2017-01-16 10:39:16 +0700

    completing home

--- src/main/java/controller/menu/CMHome.java
+++ src/main/java/controller/menu/CMHome.java
@@ -1,10 +1,26 @@
 package controller.menu;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import controller.school.CSchoolList;
+import controller.school.CSchoolNew;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import model.util.Session;
+import model.util.pattern.observer.ObservableDBSchool;
+import org.jetbrains.annotations.NotNull;
+import view.school.ISchoolList;
+import view.school.ISchoolNew;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Observer;
+import java.util.ResourceBundle;
 
 /*
  * This <skripsi.hdpso.scheduling> project in package <controller> created by : 
@@ -15,13 +31,62 @@ import javafx.scene.Node;
  */
 public class CMHome implements Initializable
 {
-    @Override public void initialize(URL location, ResourceBundle resources)
-    {
+    public Button buttonNew;
+    public Button buttonLoad;
+    private BooleanProperty disableListener;
+
+    public CMHome() {
 
     }
 
-    public void onButtonExitPressed(ActionEvent event)
+    @Override public void initialize(URL location, ResourceBundle resources)
     {
-        ((Node) event.getSource()).getScene().getWindow().hide();
+        if (!Session.getInstance().containsKey("school")) {
+            Session.getInstance().put("school", new ObservableDBSchool(null));
+        }
+        this.disableListener = new SimpleBooleanProperty(true);
+        this.buttonNew.disableProperty().bindBidirectional(new SimpleBooleanProperty());
+        @NotNull final Observer observer = (o, arg) -> {
+            if (o instanceof ObservableDBSchool) {
+                this.disableListener.setValue(((ObservableDBSchool) o).getSchool() != null);
+            }
+        };
+
+        this.buttonNew.disableProperty().bind(this.disableListener);
+        this.buttonLoad.disableProperty().bind(this.disableListener);
+        ((ObservableDBSchool) Session.getInstance().get("school")).addObserver(observer);
+    }
+
+    public void onButtonExitPressed(ActionEvent actionEvent)
+    {
+        ((Node) actionEvent.getSource()).getScene().getWindow().hide();
+    }
+
+    public void onButtonCreateNewPressed(ActionEvent actionEvent) {
+        @NotNull final Stage dialog = new Stage();
+        dialog.setTitle("Create new Timetable");
+
+        try {
+            dialog.setScene(new Scene(ISchoolNew.load(new CSchoolNew()).load()));
+        } catch (IOException ignored) {
+        }
+
+        dialog.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.showAndWait();
+    }
+
+    public void onSchoolListPressed(ActionEvent actionEvent) {
+        @NotNull final Stage dialog = new Stage();
+        dialog.setTitle("Load Timetable");
+
+        try {
+            dialog.setScene(new Scene(ISchoolList.load(new CSchoolList()).load()));
+        } catch (IOException ignored) {
+        }
+
+        dialog.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.showAndWait();
     }
 }
