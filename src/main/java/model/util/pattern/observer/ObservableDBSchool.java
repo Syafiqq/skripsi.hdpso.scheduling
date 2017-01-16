81584cdd709437faf882fcdac1308a9b79ca969b
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2017-01-16 10:42:37 +0700

    Add observable dbSchool

---
+++ src/main/java/model/util/pattern/observer/ObservableDBSchool.java
@@ -0,0 +1,40 @@
+package model.util.pattern.observer;
+/*
+ * This <skripsi.hdpso.scheduling> project created by : 
+ * Name         : Muhammad Syafiq
+ * Date / Time  : 16 January 2017, 8:53 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+import model.database.component.DBSchool;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ObservableDBSchool extends Observable {
+    @Nullable
+    private DBSchool school;
+
+    public ObservableDBSchool(@Nullable DBSchool school) {
+        this.setSchool(school);
+    }
+
+    @Nullable
+    public DBSchool getSchool() {
+        return school;
+    }
+
+    public void setSchool(@Nullable DBSchool school) {
+        this.school = school;
+        this.setChanged();
+        this.notifyObservers();
+    }
+
+    @Override
+    public synchronized void addObserver(Observer o) {
+        super.addObserver(o);
+        o.update(this, null);
+    }
+}
