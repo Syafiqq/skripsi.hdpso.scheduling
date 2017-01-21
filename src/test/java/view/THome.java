c1d9dbf3df6053ea0f5800053af3cc3f7568861f
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2017-01-21 15:02:21 +0700

    pre defined school

--- src/test/java/view/THome.java
+++ src/test/java/view/THome.java
@@ -9,14 +9,40 @@ package view;
  */
 
 import javafx.application.Application;
+import model.database.component.DBSchool;
+import model.database.core.DBType;
+import model.database.model.MSchool;
+import model.method.pso.hdpso.component.Setting;
+import model.util.Session;
+import model.util.pattern.observer.ObservableDBSchool;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
 
 @SuppressWarnings("Duplicates") public class THome
 {
     @BeforeClass
     public static void setUpClass() throws InterruptedException
     {
+        try {
+            @NotNull final MSchool mSchool = new MSchool(Setting.getDBUrl(Setting.defaultDB, DBType.DEFAULT));
+
+            @Nullable DBSchool school = mSchool.select(2);
+            if (school != null) {
+                if (!Session.getInstance().containsKey("school")) {
+                    Session.getInstance().put("school", new ObservableDBSchool(school));
+                } else {
+                    ((ObservableDBSchool) Session.getInstance().get("school")).setSchool(school);
+                }
+            }
+        } catch (SQLException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
         Thread t = new Thread()
         {
             public void run()
