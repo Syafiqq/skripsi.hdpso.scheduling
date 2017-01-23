46e1b437ed9def6deec0f7b528a6979d6924df30
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2017-01-23 15:54:09 +0700

    Reworked Session data

--- src/main/java/model/util/Session.java
+++ src/main/java/model/util/Session.java
@@ -9,9 +9,14 @@ package model.util;/*
 import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
 
 @SuppressWarnings({"SuspiciousMethodCalls", "unused"})
-public class Session {
+public class Session extends Observable{
     private static Session ourInstance = new Session();
 
     @NotNull
@@ -25,19 +30,44 @@ public class Session {
         return ourInstance;
     }
 
-    public Object put(String key, Object value) {
-        return sessions.put(key, value);
+    /** Associates the specified value with the specified key in this function (optional operation).
+     *
+     * @param key the key.
+     * @param value the value.
+     * @return the old value, or <code>null</code> if no value was present for the given key.
+     * @see Map#put(Object, Object)
+     */
+    @Nullable public Object put(String key, Object value) {
+        @Nullable final Object obj = this.sessions.put(key, value);
+        this.setChanged();
+        this.notifyObservers(key);
+        return obj;
+    }
+
+    /** Removes this key and the associated value from this function if it is present (optional operation).
+     *
+     * @param key the key.
+     * @return the old value, or <code>null</code> if no value was present for the given key.
+     * @see Map#remove(Object)
+     */
+    @Nullable public Object remove(Object key) {
+        @Nullable final Object obj = this.sessions.remove(key);
+        this.setChanged();
+        this.notifyObservers(key);
+        return obj;
     }
 
     public Object get(Object key) {
-        return sessions.get(key);
+        return this.sessions.get(key);
     }
 
     public boolean containsKey(Object key) {
-        return sessions.containsKey(key);
+        return this.sessions.containsKey(key);
     }
 
-    public Object getOrDefault(Object key, Object defaultValue) {
-        return this.sessions.getOrDefault(key, defaultValue);
+    @Override
+    public synchronized void addObserver(Observer o) {
+        super.addObserver(o);
+        o.update(this, null);
     }
 }
