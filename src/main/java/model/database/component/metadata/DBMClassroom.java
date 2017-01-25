16bc1927d37f0b62cdfd225df1995d972fd73be4
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2017-01-25 08:41:44 +0700

    add classroom metadata

---
+++ src/main/java/model/database/component/metadata/DBMClassroom.java
@@ -0,0 +1,40 @@
+package model.database.component.metadata;
+/*
+ * This <skripsi.hdpso.scheduling> project created by : 
+ * Name         : Muhammad Syafiq
+ * Date / Time  : 25 January 2017, 7:40 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class DBMClassroom {
+    protected final int id;
+    protected String name;
+
+    public DBMClassroom(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("name", name)
+                .toString();
+    }
+}
