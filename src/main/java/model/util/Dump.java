a9f728e3c38583f8c9e2550a98665dbad205c3d2
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2017-01-28 07:47:52 +0700

    Change to primary timetable

--- src/main/java/model/util/Dump.java
+++ src/main/java/model/util/Dump.java
@@ -18,8 +18,8 @@ public class Dump {
         System.err.println("This is for Testing Only");
         System.err.println("I Warned You");
         //this.schoolMetadata = new DBSchool(1, "Program Teknologi Informasi dan Ilmu Komputer Universitas Brawijaya", "PTIIK UB", "Jl. Veteran No.8, Ketawanggede, Kec. Lowokwaru, Kota Malang, Jawa Timur", "2013 - 2014", 0, 17, 5);
-        //Dump.school = new DBMSchool(1, "Program Teknologi Informasi dan Ilmu Komputer Universitas Brawijaya", "2013 - 2014", 0, 17, 5);
-        Dump.school = new DBMSchool(2, "A", "B", 1, 17, 5);
+        Dump.school = new DBMSchool(1, "Program Teknologi Informasi dan Ilmu Komputer Universitas Brawijaya", "2013 - 2014", 0, 17, 5);
+        //Dump.school = new DBMSchool(2, "A", "B", 1, 17, 5);
         return Dump.school;
     }
 }
