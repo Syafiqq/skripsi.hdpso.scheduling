bae04a1e3f029db2f00169cf8cb81d7498531e82
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2017-02-01 11:22:57 +0700

    Add Value Observer

--- src/main/java/model/custom/java/util/ValueObserver.java
+++ src/main/java/model/custom/java/util/ValueObserver.java
@@ -3,11 +3,11 @@ package model.custom.java.util;
 /*
  * This <skripsi.hdpso.scheduling> project in package <model.custom.java.util> created by : 
  * Name         : Muhammad Syafiq
- * Date / Time  : 28 January 2017, 10:24 PM.
+ * Date / Time  : 31 January 2017, 11:10 PM.
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
-public interface ParticleObserver
+public interface ValueObserver
 {
-    void update(double val);
+    void update(double... val);
 }
